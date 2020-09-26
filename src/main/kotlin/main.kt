4dba8366daa39997cb60b3e6c5d0c49d0d6f2acf
Henk van Asselt <Henk.van.Asselt@gmail.com>
2020-09-26 17:58:42 +0200

    Improved functionality of checkADB()

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,22 +7,51 @@ val adb_exe = adb.absolutePath
 
 fun main(args: Array<String>) {
 
-
-//    println(checkADB())
+    if (!checkADB()) {
+        println("Error: Cannot continue due to error in ADB")
+        return
+    }
     val path = "/sdcard/honeywell/"
-    var ret = exec(mutableListOf(adb_exe, "shell", "ls", "-l", "-R", path))
+    var ret = exec(mutableListOf(adb_exe, "shell", "ls", "-l", path))
     println(ret)
 }
 
 /**
  * Check if a device is connected over ADB
- * return: true if a device was found, false if not
+ * return: true if ADB is active and a connected device was found
  */
 fun checkADB(): Boolean {
+    if (!adb.isFile) {
+        println("Error: Could not find " + adb)
+        return false
+    }
+
+    // Evaluate and react on the response of "adb devices"
+    // There has to be at least one device connected and usable
     val ret = exec(mutableListOf(adb_exe, "devices"))
     when {
-        "no devices/emulators found" in ret -> return false
+        "no devices/emulators found" in ret -> {
+            println(ret)
+            return false
+        }
+        "List of devices attached" == ret.trim() -> {
+            println("No device(s) are connected")
+            return false
+        }
+        "unauthorized" in ret -> {
+            println(ret)
+            return false
+        }
+        "no permission" in ret -> {
+            println(ret)
+            return false
+        }
+        "\tdevice" in ret -> {
+            // At least one device was found
+            return true
+        }
     }
+    println("Warning: ADB response to validate: " + ret)
     return true
 }
 
