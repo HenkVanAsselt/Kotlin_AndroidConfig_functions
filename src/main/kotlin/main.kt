6b1c8a6a6ecbc3dee67cfbafe778d55506a770cc
Henk van Asselt <Henk.van.Asselt@gmail.com>
2020-10-07 18:29:45 +0200

    Added an readme.MD file, a Windows CMD file to test the .JAR file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,63 +2,64 @@
 
 fun main(args: Array<String>) {
 
-    print("Commandline arguments ")
+    // Determine the number of commandline arguments. Used for slicing later.
     val argCount = args.size
-    println("len = $argCount")
 
-    // Print the arguments. @todo Remove this test output
-    if (argCount > 0) {
-        for (s in args) {
-            println(s)
-        }
+    // Test if any commandline arguments are given. If there are, print them.
+    if (argCount == 0) {
+        println("No Commandline arguments given")
+        return // 0x00000005   // ERROR_ACCESS_DENIED
     }
     else {
-        println("No Commandline arguments given")
-        return
+        for (s in args) print("$s ")
+        println()
     }
 
+    // Create a slice of commandline arguments, removing the very first one.
+    // This will make it possible later to use for example 'dir' instead of 'ls'
+    // So "dir -l /sdcard/" will be processed as "ls -l /sdcard"
     var otherArgs = arrayOf("")
-
     if (argCount > 1){
-        otherArgs = args.sliceArray(1..argCount-1)
-        println("Remaining arguments:")
-        for (s in otherArgs) {
-            println(s)
-        }
-        println("===")
+        otherArgs = args.sliceArray(1 until argCount-1)
     }
-
 
     // Check if ADB.exe can be found and started
     if (!checkADB()) {
         println("Error: Cannot continue due to error in ADB")
-        return
+        return // 0x00000005   // ERROR_ACCESS_DENIED
     }
 
+    // Process the commandline arguments
     when {
         args[0] == "ls" || args[0] == "dir" -> {
-//            val ret = exec(mutableListOf(adbPath, "shell", "ls", "-l", "/sdcard/"))
             val ret = exec(mutableListOf(adbPath, "shell", "ls", *otherArgs))
             println(ret)
         }
+        args[0] == "reboot" -> {
+            // Arguments could be:  reboot, reboot bootloader, reboot recovery
+            val ret = exec(mutableListOf(adbPath, *args))
+            println(ret)
+        }
+        args[0] == "restart" -> {
+            println("Step 1: killing adb service")
+            var ret = exec(mutableListOf(adbPath, "kill-server"))
+            println(ret)
+            println("Step 2: restarting adb.exe")
+            ret = exec(mutableListOf(adbPath, "start-server"))
+            println(ret)
+            if ("daemon started successfully" in ret) {
+                return // 0x00000000  // ERROR_SUCCESS
+            }
+            return // 0x00000015 // ERROR_NOT_READY
+        }
         else -> {
-            println("Unkown argument " + '"' + args[0] + '"')
-            return
+//            println("Unkown argument " + '"' + args[0] + '"')
+//            return
+            // Just try if this is a valid shell command....
+            val ret = exec(mutableListOf(adbPath, "shell", *args))
+            println(ret)
+            return // 0x00000000  // ERROR_SUCCESS
         }
     }
-
-//    // test ls. @todo Remove this test from here
-//    val ret = exec(mutableListOf(adbPath, "shell", "ls", "-l", "/sdcard/"))
-//    println(ret)
-
+    return // 0x00000001   // ERROR_INVALID_FUNCTION
 }
-
-
-
-
-
-
-
-
-
-
--- testjar.cmd
+++ testjar.cmd
@@ -1,2 +1,6 @@
-java -jar D:\hva\projects\Kotlin_AndroidConfig_functions\build\libs\Kotlin_AndroidConfig_functions-1.0-SNAPSHOT.jar %1 %2 %3
+java -jar .\build\libs\Kotlin_AndroidConfig_functions-1.0-SNAPSHOT.jar %1 %2 %3
+echo Error level = %ErrorLevel%
+
+@echo off
+rem java -jar D:\hva\projects\Kotlin_AndroidConfig_functions\build\libs\Kotlin_AndroidConfig_functions-1.0-SNAPSHOT.jar %1 %2 %3
 
